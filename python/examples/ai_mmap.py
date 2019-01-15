olsonse/comedilib

commit 37a09df32c7aea32e123b0c8f3515bac5b5bded1

fix/improve examples for new python interface
Enable all examples to be more easily used from another script (for
testing).

These all now seem to work.  There may be something still wrong with the
analog input examples (insn.py and ai_mmap.py):  insn.py elicits a timeout
error from the driver and ai_mmap.py appears to function, but does not
write any data.

python/examples/ai_mmap.py
@@ -19,7 +19,7 @@ The point of this example was to show a simple method of reading from an analog
 input channel using the mmap data interface.
 """
 
-import comedi
+import comedi, sys, time
 from comedi import CR_PACK, TRIG_EXT
 import numpy as np
 from ctypes import c_uint, c_ubyte, sizeof
@@ -32,26 +32,36 @@ arefs = dict(
   other   = comedi.AREF_OTHER,
 )
 
+command_test_errors = {
+  1: 'unsupported trigger in ..._src setting of comedi command, setting zeroed',
+  2: '..._src setting not supported by driver',
+  3: 'TRIG argument of comedi command outside accepted range',
+  4: 'adjusted TRIG argument in comedi command',
+  5: 'chanlist not supported by board',
+}
+
 def get_subdev_status(dev, subdev):
   flags = comedi.get_subdevice_flags(dev,subdev)
   return comedi.extensions.subdev_flags.to_dict(flags)
 
 
-def main(device='/dev/comedi0', subdevice=-1, channel=0, trigger=None,
-         aref='ground', clock='timer', frequency=1000., n_samples = 1000,
-         output='output.txt'):
+def run(device='/dev/comedi0', subdevice=-1, channel=0, trigger=None,
+         aref='ground', clock='timer', clock_rate=1000., settling_time_ns=10000,
+         n_samples = 1000, output='output.txt'):
   outf = open(output, 'w')
 
   dev = comedi.open(device)
 
   # set up the command
   cmd = comedi.cmd()
+  aref = arefs[ aref ]
   cmd.chanlist = (c_uint * 1)(CR_PACK(channel, rng=0, aref=aref))
   cmd.chanlist_len = 1
   cmd.flags &= ~comedi.CMDF_WRITE
   if subdevice < 0:
-    subdevice = comedi.find_subdevice_by_type(dev, comedi.SUBD_AO, 0)
+    subdevice = comedi.find_subdevice_by_type(dev, comedi.SUBD_AI, 0)
   cmd.subdev = subdevice
+  comedi.set_read_subdevice(dev, cmd.subdev)
 
   if trigger is None:
     cmd.start_src = comedi.TRIG_NOW
@@ -62,13 +72,13 @@ def main(device='/dev/comedi0', subdevice=-1, channel=0, trigger=None,
 
   if clock == 'timer':
     cmd.scan_begin_src = comedi.TRIG_TIMER
-    cmd.scan_begin_arg = int(1e9 / frequency)
+    cmd.scan_begin_arg = int(1e9 / clock_rate)
   else:
     cmd.scan_begin_src = comedi.TRIG_EXT
     cmd.scan_begin_arg = comedi.CR_EDGE | clock
 
   cmd.convert_src = comedi.TRIG_TIMER
-  cmd.convert_arg = 1
+  cmd.convert_arg = int(settling_time_ns)
 
   cmd.scan_end_src = comedi.TRIG_COUNT
   cmd.scan_end_arg = 1 # == number of channels
@@ -77,13 +87,17 @@ def main(device='/dev/comedi0', subdevice=-1, channel=0, trigger=None,
   cmd.stop_arg = n_samples
 
   # test the command
+  i = comedi.cmd.from_buffer_copy(cmd)
   err = comedi.command_test(dev, cmd)
   if err < 0:
     raise RuntimeError('comedi.command_test: {}'
                        .format(comedi.strerror(comedi.errno())))
-  elif err:
-    raise RuntimeError('comedi.command_test:  non-zero value {}'.format(err))
-
+  if err in [1, 2, 3, 5]:
+    if repr(i) != repr(cmd):
+      E = 'differences: {}'.format(i.diff(cmd))
+    else:
+      E = 'no differences ???\n{}'.format(cmd)
+    raise RuntimeError(command_test_errors[err] + ':\n\t' + E)
 
   # set up the memory mapping
   # kind of memory we need to map...
@@ -141,7 +155,7 @@ def main(device='/dev/comedi0', subdevice=-1, channel=0, trigger=None,
 
 
 
-if __name__ == '__main__':
+def process_args(arglist):
   import argparse
 
   parser = argparse.ArgumentParser(description=__doc__)
@@ -160,13 +174,21 @@ if __name__ == '__main__':
          'These can be any item that is reasonable (better know your hardware) '
          'and resolvable using the comedi module namespace.  For example, '
          'NI_PFI(0), TRIGGER_LINE(1), ... are valid for NI devices.')
-  parser.add_argument('--clock', type=str, default='timer', help='[timer]')
+  parser.add_argument('--clock', type=str, default='timer', help='[Default timer]')
   parser.add_argument('--clock_rate', type=float, default=1000., help='[1000.]')
+  parser.add_argument('--settling_time_ns', type=int, default=10000,
+    help='Specify the settling time for each conversion in ns [Default 10000].  '
+         'Note that the minimum time used for allowing the ADC to settle is '
+         'limited by the actual hardware.  In other words, if you select an '
+         'invalid value, a runtime exception will be thrown.')
   parser.add_argument('-N', '--n_samples', type=int, default=1000, help='[1000]')
   parser.add_argument('-o', '--output', default='output.txt',
     help='path to output file')
 
-  O = parser.parse_args()
+  return parser.parse_args(arglist)
+
+def main(arglist):
+  O = process_args(arglist)
 
   try:
     if O.trigger:
@@ -177,4 +199,7 @@ if __name__ == '__main__':
   except NameError as n:
     print 'Name Error: ', n
 
-  main(**O.__dict__)
+  run(**O.__dict__)
+
+if __name__ == '__main__':
+  main(sys.argv[1:])

python/comedi/extensions/cmd.py
@@ -21,8 +21,15 @@ def comedi_cmd_to_dict(self):
 
   return D
 
+def comedi_cmd_diff(self, other):
+  setI = lambda L : set([(i[0], repr(i[1])) for i in L])
+  old = setI(self.dict().items()) - setI(other.dict().items())
+  new = setI(other.dict().items()) - setI(self.dict().items())
+  return dict(old=dict(old), new=dict(new))
+
 def comedi_cmd_repr(self):
   return repr( self.dict() )
 
 comedi.comedi_cmd.dict = comedi_cmd_to_dict
+comedi.comedi_cmd.diff = comedi_cmd_diff
 comedi.comedi_cmd.__repr__ = comedi_cmd_repr

python/examples/ao_dds.py
@@ -274,13 +274,19 @@ class Test(object):
 
     self.trigger()
 
+  @property
+  def status(self):
+    self.subdevice_flags = \
+      comedi.get_subdevice_flags(self.dev, self.options.subdevice)
+    return comedi.extensions.subdev_flags.to_dict( self.subdevice_flags )
+
   def wait(self):
     """
     wait while the waveform executes
     """
     try:
       if not self.options.write_more:
-        while True:
+        while self.status.running:
           time.sleep(1)
       elif self.options.oswrite:
         # FIXME: this is not quite generic for both enough yet.  Need to call
@@ -452,19 +458,25 @@ dds_list = [
 ]
 
 
-def process_args(argv):
+def process_args(arglist):
   parser = argparse.ArgumentParser(description=__doc__)
   parser.add_argument( '-f', '--filename', nargs='?', default='/dev/comedi0',
-    help='Comedi device file' )
-  parser.add_argument( '-s', '--subdevice', type=int, default=-1 )
-  parser.add_argument( '-c', '--channels', nargs='+', type=int, default=[0] )
-  parser.add_argument( '-a', '--aref',  choices=['ground', 'common'], default='ground' )
-  parser.add_argument( '-r', '--range', type=int, default=0 )
-  parser.add_argument( '-N', '--n_samples', type=int, default=0 )
+    help='Comedi device file [Default: /dev/comedi0]' )
+  parser.add_argument( '-s', '--subdevice', type=int, default=-1,
+    help='Select analog output subdevice [Default:  <first AO>]' )
+  parser.add_argument( '-c', '--channels', nargs='+', type=int, default=[0],
+    help='Select output channels [Default: [0]]' )
+  parser.add_argument( '-a', '--aref',  choices=['ground', 'common'],
+    default='ground',
+    help='Select analog reference [Default: ground]' )
+  parser.add_argument( '-r', '--range', type=int, default=0,
+    help='Select analog output range number [Default 0]' )
+  parser.add_argument( '-N', '--n_samples', type=int, default=0,
+    help='Specify number of output samples per channel [Default 0(all-memory)]' )
   parser.add_argument( '-W', '--waveform_freq', type=float, default=10.0,
-    help='set waveform frequency [default 10.0]' )
+    help='set waveform frequency in Hz [default 10.0]' )
   parser.add_argument( '-F', '--freq', type=float, default=1000.,
-    help='set update frequency [default 1000.]' )
+    help='set update frequency in Hz [default 1000.]' )
   parser.add_argument( '-U', '--update_source',
     default='timer',
     help='Select update signal source [Default: timer].  '
@@ -474,22 +486,24 @@ def process_args(argv):
          'NI_PFI(0), TRIGGER_LINE(1), ... are valid for NI devices.  You can '
          'also specify a raw integer value if desired.')
   parser.add_argument( '-w', '--waveform', type=int, default=0,
-    help='\n\t'.join([ '{}: {}'.format(i,c.name)
+    help='[Default 0]' + '\n\t'.join([ '{}: {}'.format(i,c.name)
            for i,c in zip(xrange(len(dds_list)), dds_list)]) )
-  parser.add_argument( '-C', '--continuous', action='store_true' )
+  parser.add_argument( '-C', '--continuous', action='store_true',
+    help='Select continuous operation')
   parser.add_argument( '-v', '--verbose', action='store_true' )
   parser.add_argument( '--oswrite', action='store_true' )
   parser.add_argument( '--write_more', action='store_true' )
-  parser.add_argument( '-L', '--waveform_len', type=int, default=1<<16 )
+  parser.add_argument( '-L', '--waveform_len', type=int, default=1<<16,
+    help='Select the number of samples in the waveform to repeat' )
   parser.add_argument( '-S', '--show-waveform', action='store_true' )
-  return parser.parse_args(argv)
+  return parser.parse_args(arglist)
 
-def main(argv):
-  t = Test( process_args(argv) )
+def main(arglist):
+  t = Test( process_args(arglist) )
   t.start()
   t.wait()
   t.stop()
   t.close()
 
 if __name__ == '__main__':
-  main(sys.argv)
+  main(sys.argv[1:])

python/examples/ao_tests.py
@@ -21,21 +21,31 @@ The point of this example was to show a simple method of writing to an analog
 output channel using the os.write data interface.
 """
 
+import os, sys, time
 import comedi
-from comedi import CR_PACK, TRIG_EXT
-from ctypes import c_uint
+from comedi import CR_PACK
+from ctypes import c_uint, c_ubyte, sizeof
 
 import numpy as np
 import argparse
+
+command_test_errors = {
+  1: 'unsupported trigger in ..._src setting of comedi command, setting zeroed',
+  2: '..._src setting not supported by driver',
+  3: 'TRIG argument of comedi command outside accepted range',
+  4: 'adjusted TRIG argument in comedi command',
+  5: 'chanlist not supported by board',
+}
 
 def get_subdev_status(dev, subdev):
   flags = comedi.get_subdevice_flags(dev,subdev)
   return comedi.extensions.subdev_flags.to_dict(flags)
 
 
-def main(trigger=None, clock='timer', frequency=1000., continuous=True,
-         samples_per_channel = 1000):
-  dev = comedi.open('/dev/comedi1')
+def run(device='/dev/comedi0', trigger=None, clock='timer',
+        frequency=1000., continuous=True,
+        samples_per_channel = 1000):
+  dev = comedi.open(device)
 
   cmd = comedi.cmd()
   # only channel 0
@@ -43,6 +53,7 @@ def main(trigger=None, clock='timer', frequency=1000., continuous=True,
   cmd.chanlist_len = 1
   cmd.flags = comedi.CMDF_WRITE
   cmd.subdev = comedi.find_subdevice_by_type(dev, comedi.SUBD_AO, 0)
+  comedi.set_write_subdevice(dev, cmd.subdev)
 
   if trigger is None:
     cmd.start_src = comedi.TRIG_INT
@@ -58,6 +69,9 @@ def main(trigger=None, clock='timer', frequency=1000., continuous=True,
     cmd.scan_begin_src = comedi.TRIG_EXT
     cmd.scan_begin_arg = comedi.CR_EDGE | clock
 
+  cmd.convert_src = comedi.TRIG_NOW
+  cmd.convert_arg = 0
+
   cmd.scan_end_src = comedi.TRIG_COUNT
   cmd.scan_end_arg = 1 # == number of channels
 
@@ -65,12 +79,17 @@ def main(trigger=None, clock='timer', frequency=1000., continuous=True,
   cmd.stop_arg = samples_per_channel
 
   # test the command
+  i = comedi.cmd.from_buffer_copy(cmd)
   err = comedi.command_test(dev, cmd)
   if err < 0:
     comedi.perror("comedi.command_test")
     raise RuntimeError()
-  elif err:
-    raise RuntimeError('comedi.command_test:  non-zero value {}'.format(err))
+  if err in [1, 2, 3, 5]:
+    if repr(i) != repr(cmd):
+      E = 'differences: {}'.format(i.diff(cmd))
+    else:
+      E = 'no differences ???\n{}'.format(cmd)
+    raise RuntimeError(command_test_errors[err] + ':\n\t' + E)
 
   # initiate command
   err = comedi.command(dev, cmd)
@@ -85,11 +104,13 @@ def main(trigger=None, clock='timer', frequency=1000., continuous=True,
   data = generate_data(samples_per_channel, num_channels=1, sampl_t=sampl_t)
 
   # and finally write the data
-  buf = (c_ubyte * samples_per_channel).from_buffer(data)
-  os.write(comedi.fileno(dev), buf)
+  buf = (c_ubyte * (sizeof(sampl_t) * samples_per_channel)).from_buffer(data)
+  L = os.write(comedi.fileno(dev), buf)
+  if L != len(buf):
+    raise OSError('could only write {} out of {} bytes'.format(L, len(buf)))
 
   # trigger the command start
-  if cmd.start_src == TRIG_INT:
+  if cmd.start_src == comedi.TRIG_INT:
     ret = comedi.internal_trigger(dev, cmd.subdev, 0)
     if ret < 0:
       comedi.perror('comedi.internal_trigger error')
@@ -105,11 +126,12 @@ def main(trigger=None, clock='timer', frequency=1000., continuous=True,
     pass
 
   # finally, close the device:
+  comedi.cancel(dev, cmd.subdev)
   comedi.close(dev)
 
 
 def generate_data(samples_per_channel, num_channels, sampl_t, mx=4*np.pi):
-  shape = ( self.samples_per_channel, len(options.channels) )
+  shape = ( samples_per_channel, num_channels )
   data = np.zeros( shape, dtype=sampl_t )
   t = np.r_[0:mx:1j*samples_per_channel]
   for i in xrange(num_channels):
@@ -117,8 +139,10 @@ def generate_data(samples_per_channel, num_channels, sampl_t, mx=4*np.pi):
   return data
 
 
-if __name__ == '__main__':
+def process_args(arglist):
   parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument( '-f', '--filename', nargs='?', default='/dev/comedi0',
+    help='Comedi device file [Default: /dev/comedi0]' )
   parser.add_argument('--trigger', type=str, default=None,
     help='Select trigger [Default: None].  '
          'These can be any item that is reasonable (better know your hardware) '
@@ -129,7 +153,10 @@ if __name__ == '__main__':
   parser.add_argument('--continuous', action='store_true', help='[False]')
   parser.add_argument('--waveform_len', type=int, default=1000, help='[1000]')
 
-  O = parser.parse_args()
+  return parser.parse_args(arglist)
+
+def main(arglist):
+  O = process_args(arglist)
 
   try:
     if O.trigger:
@@ -140,4 +167,8 @@ if __name__ == '__main__':
   except NameError as n:
     print 'Name Error: ', n
 
-  main(O.trigger, O.clock, O.clock_rate, O.continuous, O.waveform_len)
+  run(O.filename, O.trigger, O.clock, O.clock_rate, O.continuous,
+      O.waveform_len)
+
+if __name__ == '__main__':
+  main(sys.argv[1:])

python/examples/info.py
@@ -70,14 +70,14 @@ def main(devfile = '/dev/comedi0'):
       n_ranges = comedi.get_n_ranges(dev,i,0)
       print "\t\tall chans:"
       for j in range(n_ranges):
-        rng = comedi.get_range(dev,i,0,j)
+        rng = comedi.get_range(dev,i,0,j).contents
         print "\t\t[%g,%g]" % (rng.min, rng.max)
     else:
       for chan in range(n_chans):
         n_ranges = comedi.get_n_ranges(dev,i,chan)
         print "\t\tchan: %d" % (chan)
         for j in range(n_ranges):
-          rng = comedi.get_range(dev,i,chan,j)
+          rng = comedi.get_range(dev,i,chan,j).contents
           print "\t\t[%g,%g]" % (rng.min, rng.max)
     print "\tcommand:"
     get_command_stuff(dev,i)
@@ -165,7 +165,8 @@ if __name__ == '__main__':
   import argparse
 
   parser = argparse.ArgumentParser()
-  parser.add_argument('--devfile', type=str, default=None, help='[None]')
+  parser.add_argument('--devfile', type=str, default='/dev/comedi0',
+    help='Select the comedi device file [Default: /dev/comedi0]')
   O = parser.parse_args()
   try:
     main(O.devfile)

python/examples/insn.py
@@ -18,7 +18,7 @@ This example does 3 instructions in one system call.  It does a
 input, and the another `gettimeofday()` call.
 """
 
-import comedi
+import comedi, sys
 from ctypes import c_uint32
 
 
@@ -55,22 +55,23 @@ def setup_read_insn(dev, subdev, chan, range, aref, n_scan, insn):
   insn.n = n_scan
   insn.data = (c_uint32 * n_scan)()
   insn.subdev = subdev
-  insn.chanspec = comedi.CR_PACK(channel, range, aref)
+  insn.chanspec = comedi.CR_PACK(chan, range, aref)
 
 
-def main(filename, ai_subdev, ai_chan, ai_range, ai_ref, ai_scans):
+def run(args):
   dev = comedi.open(args.filename)
   if not dev:
     raise RuntimeError('error opening Comedi device {}'.format(args.filename))
 
-  if ai_subdev < 0:
-    ai_subdev = comedi.find_subdevice_by_type(dev, comedi.SUBD_AI, 0)
+  if args.ai_subdev < 0:
+    args.ai_subdev = comedi.find_subdevice_by_type(dev, comedi.SUBD_AI, 0)
 
   # prepare the instructions
   insns = comedi.insnlist()
   insns.set_length(3)
   setup_gtod_insn(insns[0])
-  setup_read_insn(dev, ai_subdev, ai_chan, ai_range, ai_ref, ai_scans, insns[1])
+  setup_read_insn(dev, args.ai_subdev, args.ai_chan, args.ai_range, args.ai_ref,
+                  args.ai_scans, insns[1])
   setup_gtod_insn(insns[2])
 
   # execute
@@ -97,7 +98,7 @@ def main(filename, ai_subdev, ai_chan, ai_range, ai_ref, ai_scans):
     print(insns[1].data[i])
 
 
-if __name__ == '__main__':
+def process_args(arglist):
   import argparse
 
   def check_nscans(val):
@@ -111,24 +112,29 @@ if __name__ == '__main__':
     '-f', '--filename', default='/dev/comedi0',
     help='path to comedi device file')
   parser.add_argument(
-    '-s', '--subdevice', type=int, default=-1,
+    '-s', '--subdevice', type=int, default=-1, dest='ai_subdev',
     help='subdevice for analog input [Default=-1].  If <0, find ai subdevice')
   parser.add_argument(
-    '-c', '--channel', type=int, default=0,
+    '-c', '--channel', type=int, default=0, dest='ai_chan',
     help='channel for analog input')
   parser.add_argument(
-    '-a', '--analog-reference', dest='aref', default='ground',
+    '-a', '--analog-reference', default='ground', dest='ai_ref',
     choices=arefs.viewkeys(), help='reference for analog input')
   parser.add_argument(
-    '-r', '--range', type=int, default=0, help='range for analog input')
+    '-r', '--range', type=int, default=0, dest='ai_range',
+    help='range for analog input')
   parser.add_argument(
-    '-N', '--num-scans', type=check_nscans, default=10,
+    '-N', '--num-scans', type=check_nscans, default=10, dest='ai_scans',
     help='number of analog input scans (select 1..1000)')
 
-  args = parser.parse_args()
+  return parser.parse_args(arglist)
 
+def main(arglist):
+  args = process_args(arglist)
   try:
-    main(args.filename, args.subdevice, args.channel, args.range, args.aref,
-         args.num_scans)
+    run(args)
   except RuntimeError, e:
     print('error: {}'.format(e))
+
+if __name__ == '__main__':
+  main(sys.argv[1:])

commit 012037b070119f828af50f6ca0da64afa5cda564

python/examples: fix ai_mmap.py--now returns data
Fixes: 37a09df32c7a so that ai_mmap.py returns data.  Problem had been that
the inner loop was waiting on subdevice running status and was exiting
before the data was ready to be read.  This patch causes the inner loop to
wait on subdevice busy status which is true even if the analog acquisition
is finished--as long as there is actually data to read from the
(potentially) already finished acquisition.

python/examples/ai_mmap.py
@@ -128,7 +128,7 @@ def run(device='/dev/comedi0', subdevice=-1, channel=0, trigger=None,
   try:
     end = begin = 0
     ssize = size / sizeof(sampl_t)
-    while get_subdev_status(dev, cmd.subdev).running:
+    while get_subdev_status(dev, cmd.subdev).busy:
       end += int(comedi.get_buffer_contents(dev,subdevice) / sizeof(sampl_t))
 
       I, F = int(begin / ssize), int(end / ssize)